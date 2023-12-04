40d7bebf4e034a4eb35e9e0f6747c5221091aa9b
Fabrizio Crisafulli <thewiper@gmail.com>
2023-12-04 21:11:34 +0100

    fabcms-6 [TEMPLATE] boostrap 5 e attributi data-*

--- admin/modules/wiki/op_statistics.php
+++ admin/modules/wiki/op_statistics.php
@@ -15,8 +15,8 @@ $template->navBarAddItem('Statistics', 'admin.php?module=wiki&op=statistics');
 echo '
 <nav>
   <div class="nav nav-tabs" id="nav-tab" role="tablist">
-    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Optimizer</a>
-    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-analytics" role="tab" aria-controls="nav-profile" aria-selected="false">Analytics</a>
+    <a class="nav-item nav-link active" id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Optimizer</a>
+    <a class="nav-item nav-link" id="nav-profile-tab" data-bs-toggle="tab" href="#nav-analytics" role="tab" aria-controls="nav-profile" aria-selected="false">Analytics</a>
   </div>
 </nav>
 
