b6ec9532cea5f1235a454069d6b0e3779f2cf5cc
Fabrizio Crisafulli <thewiper@gmail.com>
2024-02-29 21:24:26 +0100

    fabcms-11 [WIKI] - Revisione totale

--- mod_load.php
+++ mod_load.php
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 // header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 // header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-
+// Hello Darwin!
 // A lock file exists?
 use CrisaSoft\FabCMS\module;
 use CrisaSoft\FabCMS\plugin;
